e6766aa6f72404a9577a7cd17eeff8813e84b507
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-10-19 16:33:45 +0600

    floating_number_to_integer_number_type_casting

--- main.go
+++ main.go
@@ -5,18 +5,14 @@ import "fmt"
 func main() {
 
 	// variable declaration
-	var balance int
+	var balance float64
 
 	// variable initialization
 	fmt.Printf("Enter the balance : ")
 	fmt.Scan(&balance)
 
-	// integer to floating 32 bit type casting
-	var floating32Balance = float32(balance)
-	fmt.Println("Floating 32 balance is : ", floating32Balance)
-
-	// integer to floating 64 bit type casting
-	var floating64Balance = float64(balance)
-	fmt.Println("Floating 64 balance is : ", floating64Balance)
+	// floating number to integer type casting
+	var balanceWithoutFloatingPoint = int(balance)
+	fmt.Println("Floating number to integer balance : ", balanceWithoutFloatingPoint)
 
 }
