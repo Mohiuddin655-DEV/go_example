1ce72a4a1aed5c5d4be0b56199017f81e59da5a3
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-10-19 15:45:48 +0600

    number formatting

--- main.go
+++ main.go
@@ -5,32 +5,18 @@ import "fmt"
 func main() {
 
 	// variable declaration
-	var firstName, lastName string
-	var country string
-	var age int
-	var gpa float32
+	var number int
 
-	// use two signs to get user input
-	// 1. & sign used for single user input
-	// 2. %v sign used for multiple user input
+	fmt.Printf("Enter the number : ")
+	fmt.Scan(&number)
 
-	// user input by scanf, scanln, scan (variable initialization)
-
-	fmt.Print("Enter the full name : ")
-	fmt.Scanf("%v %v", &firstName, &lastName)
-
-	fmt.Print("Enter the country name : ")
-	fmt.Scanln(&country)
-
-	fmt.Print("Enter the age : ")
-	fmt.Scanln(&age)
-
-	fmt.Print("Enter the GPA : ")
-	fmt.Scan(&gpa)
+	// types of number formatting
+	// 1. b for binary
+	// 2. o for octal
+	// 3. x for hexa
 
 	// show output
-	fmt.Printf("My name is %v %v.\n", firstName, lastName)
-	fmt.Printf("I'm currently %v years old\n", age)
-	fmt.Printf("I'm from %v\n", country)
-	fmt.Printf("I have got GPA %v in SSC\n", gpa)
+	fmt.Printf("Binary number = %b\n", number)
+	fmt.Printf("Octal  number = %o\n", number)
+	fmt.Printf("Hexa   number = %x\n", number)
 }
