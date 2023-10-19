5e4398fdf857fb7fd9397a77ef9caa795ecd641a
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-10-19 15:35:46 +0600

    user_input

--- main.go
+++ main.go
@@ -4,22 +4,33 @@ import "fmt"
 
 func main() {
 
-	// const variable declaration naming rules
-	// 1. must be use const keywork before
-	// 2. use uppercase const variables but not mandatory
+	// variable declaration
+	var firstName, lastName string
+	var country string
+	var age int
+	var gpa float32
 
-	// const variable
-	const COUNTRY = "Bangladesh"
+	// use two signs to get user input
+	// 1. & sign used for single user input
+	// 2. %v sign used for multiple user input
 
-	// variable declaration and initialization
-	firstName := "Md."
-	lastName := "Mohi-Uddin"
-	age := 23
-	gpa := 4.89
+	// user input by scanf, scanln, scan (variable initialization)
+
+	fmt.Print("Enter the full name : ")
+	fmt.Scanf("%v %v", &firstName, &lastName)
+
+	fmt.Print("Enter the country name : ")
+	fmt.Scanln(&country)
+
+	fmt.Print("Enter the age : ")
+	fmt.Scanln(&age)
+
+	fmt.Print("Enter the GPA : ")
+	fmt.Scan(&gpa)
 
 	// show output
 	fmt.Printf("My name is %v %v.\n", firstName, lastName)
 	fmt.Printf("I'm currently %v years old\n", age)
-	fmt.Printf("I'm from %v\n", COUNTRY)
+	fmt.Printf("I'm from %v\n", country)
 	fmt.Printf("I have got GPA %v in SSC\n", gpa)
 }
