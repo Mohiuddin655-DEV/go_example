fef400f930a2ba296ad7a4800211c7aea4193b62
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-10-19 14:44:33 +0600

    variable declaration - simplied level

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 
-	// Variable declaration root mode
-	var firstName string = "Md."
-	var lastName string = "Mohi-Uddin"
-	var country string = "Bangladesh"
-	var age int = 23
-	var gpa float32 = 4.89
+	// Variable declaration simplied mode
+	var firstName = "Md."
+	var lastName = "Mohi-Uddin"
+	var country = "Bangladesh"
+	var age = 23
+	var gpa = 4.89
 
 	fmt.Println("My name is ", firstName, " ", lastName, " .")
 	fmt.Println("I'm currently ", age, "years old")
