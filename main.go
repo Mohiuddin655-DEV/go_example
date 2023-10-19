28d18b049182f5b04f63d669ffaf2c8f1c4718b5
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-10-19 16:00:27 +0600

    floating number formatting

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import "fmt"
 func main() {
 
 	// variable declare and initialize
-	var message = "Hello World!"
+	var number = 3.1416
 
 	// types of string formatting
-	// 1. s for simple
-	// 2. q for qoute
+	// 1. f for integer to float
+	// 2. .2f for floating point first 2 digits
 
 	// show output
-	fmt.Printf("Simple message = %s\n", message)
-	fmt.Printf("Quote  message = %q\n", message)
+	fmt.Printf("Current value for floating type = %f\n", number)
+	fmt.Printf("Last two decimal value = %.2f\n", number)
 }
