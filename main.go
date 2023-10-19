ee7d789d7f0c1bd557df576dff21da31ca551bf5
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-10-19 15:06:45 +0600

    const variable

--- main.go
+++ main.go
@@ -4,16 +4,22 @@ import "fmt"
 
 func main() {
 
+	// const variable declaration naming rules
+	// 1. must be use const keywork before
+	// 2. use uppercase const variables but not mandatory
+
+	// const variable
+	const COUNTRY = "Bangladesh"
+
 	// variable declaration and initialization
 	firstName := "Md."
 	lastName := "Mohi-Uddin"
-	country := "Bangladesh"
 	age := 23
 	gpa := 4.89
 
 	// show output
 	fmt.Printf("My name is %v %v.\n", firstName, lastName)
 	fmt.Printf("I'm currently %v years old\n", age)
-	fmt.Printf("I'm from %v\n", country)
+	fmt.Printf("I'm from %v\n", COUNTRY)
 	fmt.Printf("I have got GPA %v in SSC\n", gpa)
 }
